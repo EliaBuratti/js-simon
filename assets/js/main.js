efdf26d5007d349521015c805f8b91637ac1e4f0
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-05 16:09:12 +0200

    create count down
    added logic countdown and print in page

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,36 @@ Esistono dei metodi per trasformare una data in millisecondi?
  */
 
 
+const countdownEl = document.querySelector('.count-down');
+const targhetTime = new Date(2023, 8, 6, 9, 30, 0, 0); //(year, monthIndex, day, hours, minutes, seconds, milliseconds)
+
+// genero il tempo rimanente in millisecondi
+const secondsMs = 1000;
+const minuteMs = secondsMs * 60 ;
+const hourMs = minuteMs * 60;
+const dayMs = hourMs * 24;
+
+const clock = setInterval(function(){
+
+    // ottengo la data attuale in millisecondi e poi faccio la differenza
+    const realTime = new Date().getTime();
+    const milliSecTime = Number(targhetTime - realTime);
+
+    if(milliSecTime > 0) {
+
+        // calcolo la differernza e la divido a seconda di quello che mi occorre
+        let secondsRemain = Math.floor( (milliSecTime % minuteMs) / secondsMs );
+        let minuteRemain = Math.floor((milliSecTime % hourMs) / minuteMs );
+        let hourRemain = Math.floor((milliSecTime % dayMs) / hourMs );
+        let dayRemain = Math.floor(milliSecTime / dayMs);
+        
+        //stampo in pagina il conto alla rovescia
+        countdownEl.innerHTML = `<h1>Alla lezione del ${targhetTime.toDateString()}</h1><h2>Mancano ${dayRemain} giorni, ${hourRemain} ore, ${minuteRemain} minuti e ${secondsRemain} secondi.</h2>`;
+    } else {
+        //quando scatta il timer fermo il loop e stampo in pagina
+        clearInterval(clock);
+        countdownEl.innerHTML = '<h1>Inizia la lezione!</h1>'
+    };
+}, 1000);
+
+
